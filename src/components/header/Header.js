e5ded9532c0f7a4b9c7e448f2fd8fccf909472ff
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-06-18 11:49:14 -0500

    Slight style changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,10 +5,10 @@ const Header = () => {
     return (
         <header id="header">
             <ul>
-                <li><a href="#header">Home</a></li>
-                <li><a href="#header">Services</a></li>
-                <li><a href="#header">About</a></li>
-                <li><a href="#header">Contact</a></li>
+                <li><a href="#banner">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </header>
     );
