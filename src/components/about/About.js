3ee8643567ee89b6a87ee6002f320cd7dbbb0a50
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-06-25 09:43:56 -0500

    Redesign about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './About.css';
 
+import Img from '../../images/group.png';
+
 const About = () => {
     return (
         <div id="about">
+            <div>
+                <img src={Img} alt="Heritage team" />
+            </div>
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
